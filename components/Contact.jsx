3ac762a47cb17f3e5c8af8ea88675e506a67293b
Asadrana123 <104757758+Asadrana123@users.noreply.github.com>
2025-11-01 13:07:32 +0530

    Refactor Contact component structure and elements

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,95 +4,118 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import Link from "next/link";
-
-import {HiOutlineChevronDoubleUp} from "react-icons/hi"
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const Contact = () => {
   return (
-    <div id='contact' className="w-full lg:h-screen">
+    <div id="contact" className="w-full lg:h-screen">
       <div className="w-full mx-auto pb-8 pt-24 px-2 max-w-[1240px]">
         <p className="w-full tracking-widest uppercase text-[#5651e5]">Contact</p>
         <h2 className="py-4">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
+          {/* Left Side */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full rounded-xl">
               <div>
                 <Image
                   className="rounded-xl hover:scale-105 py-1 ease-in duration-300"
                   src="/assets/projects/contact.jpg"
-                  height="450"
-                  width="420"
-                  alt="/"
+                  height={450}
+                  width={420}
+                  alt="contact"
                 />
               </div>
               <div>
                 <h2 className="py-4">Asad Lukman</h2>
                 <p>Web Developer || UI Designer</p>
-                <p className="py-4">I'm available for freelance and full-time positions. Let's connect and talk!</p>
+                <p className="py-4">
+                  I'm available for freelance and full-time positions. Let's connect and talk!
+                </p>
               </div>
-          
-            <div className="pt-20">
-              <p className="uppercase text-[#5651e5]"> connect with me</p>
-              <div className=" justify-between flex items-center py-4">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaLinkedin />
-                </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaGithub />
-                </div>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <AiOutlineMail />
-                </div>
-                <a href="https://drive.google.com/file/d/1aMy_7x1A5P8Xsu115cdqzE7VsWq9ttOA/view?usp=share_link" target="_b">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <BsFillPersonLinesFill />
-                </div>
+
+              {/* Social Icons */}
+              <div className="pt-20">
+                <p className="uppercase text-[#5651e5]">connect with me</p>
+                <div className="justify-between flex items-center py-4">
+                  <a
+                    href="https://www.linkedin.com/in/connectasadrana/"
+                    className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLinkedin />
                   </a>
-              </div>
+                  <a
+                    href="https://github.com/Asadrana123"
+                    className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    href="mailto:asadlukman246@gmail.com"
+                    className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <AiOutlineMail />
+                  </a>
+                  <a
+                    href="https://drive.google.com/file/d/1aMy_7x1A5P8Xsu115cdqzE7VsWq9ttOA/view?usp=share_link"
+                    className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsFillPersonLinesFill />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-                   <div className="shadow-xl shadow-gray-400 h-auto w-full rounded-xl lg:p-4 col-span-3">
-                <div className="py-4">
 
-                  <form >
-                  <div className="grid md:grid-cols-2 gap-4 py-2 w-full ">
-                 <div className="flex flex-col">
-                  <label className="py-2 uppercase text-sm mx-1">Name</label>
-                   <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
-                 </div>
-
-                 <div className="flex flex-col">
-                  <label className="py-2 uppercase text-sm mx-1">Phone Number</label>
-                   <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
-                 </div>
-                 </div>
-                      
-                      <div className="flex flex-col py-2">
-                        <label className="uppercase text-sm py-2">Email</label>
-                        <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
-                      </div>
-                      <div className="flex flex-col py-2">
-                        <label className="uppercase text-sm py-2">Subject</label>
-                        <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
-                      </div>
-                      <div className="flex flex-col py-2">
-                        <label className="text-sm uppercase py-2">message</label>
-                        <textarea className="border-2 rounded-lg p-3 border-gray-300" rows='8' ></textarea>
-                      </div>
-                          <button className="uppercase w-full p-4 mt-4 text-gray-100">send message</button>
-                 </form>
-                
-              </div>
-              </div>
+          {/* Right Side - Form Section */}
+          <div className="shadow-xl shadow-gray-400 h-auto w-full rounded-xl lg:p-4 col-span-3">
+            <div className="py-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 py-2 w-full">
+                  <div className="flex flex-col">
+                    <label className="py-2 uppercase text-sm mx-1">Name</label>
+                    <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="py-2 uppercase text-sm mx-1">Phone Number</label>
+                    <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input className="border-2 rounded-lg flex p-3 mx-1 border-gray-300" type="text" />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="text-sm uppercase py-2">message</label>
+                  <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="8"></textarea>
+                </div>
+                <button className="uppercase w-full p-4 mt-4 text-gray-100 bg-[#5651e5] cursor-pointer">
+                  send message
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-        
+
+        {/* Back to Top */}
         <div className="flex justify-center py-12">
-          <Link href='/#home'  className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-            <HiOutlineChevronDoubleUp className='text-[#5651e5]' size={20} />
+          <Link href="/#home" passHref>
+            <a className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={20} />
+            </a>
           </Link>
         </div>
-
       </div>
     </div>
   );
